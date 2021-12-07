6a935872b8db4d34dff1d75270f87f42767867f9
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2021-12-07 15:21:06 +0200

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useState } from 'react';
-import { useEffect } from 'react/cjs/react.development';
+import { useState, useEffect } from 'react';
 import './App.css';
 import AddContact from './components/AddContact';
 import ContactList from './components/ContactList';
